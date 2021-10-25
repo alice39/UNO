21caf08498d2f742ca4cae2090f0dfd5fb84c935
A.L.I.C.E <20363359+alice39@users.noreply.github.com>
2021-10-25 13:00:52 -0400

    add Card class, shuffleDesk and getRandomResk in Card.tsx

--- uno-client/src/Card.tsx
+++ uno-client/src/Card.tsx
@@ -19,7 +19,70 @@ enum CardType {
     Block,
     Reverse,
     Wild_2,
-    Wild_Color
+    Wild_Color,
+    Wild_Color_4
 };
 
-export default {CardColor, CardType};
+// represents how many times a card is repeated through all
+// colors in CardColor, number index are linked by CardType
+const cardSize: number[] = [
+    1,
+    2,
+    2,
+    2,
+    2,
+    2,
+    2,
+    2,
+    2,
+    2,
+    2,
+    2,
+    2,
+    1,
+    1
+];
+
+class Card {
+    readonly color: CardColor;
+    readonly type: CardType;
+
+    constructor(color: CardColor, type: CardType) {
+        this.color = color;
+        this.type = type;
+    }
+};
+
+// source: https://stackoverflow.com/a/2450976
+function shuffleDesk(array: Card[]): Card[] {
+    let currentIndex = array.length, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
+
+function getRandomDesk(): Card[] {
+    let desk: Card[] = new Array(108);
+    let next: number = 0;
+
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 15; j++) {
+            for (let k = 0; k < cardSize[j]; k++) {
+                desk[next++] = new Card(i, j);
+            }
+        }
+    }
+
+    return shuffleDesk(desk);
+}
+
+export default {CardColor, CardType, Card, shuffleDesk, getRandomDesk};
